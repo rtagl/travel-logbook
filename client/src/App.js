38e597b3119cb6de819a1edc55813f06e0fad640
Ric Taglietti <rictaglietti@gmail.com>
2021-08-25 15:00:33 -0400

    fetch log entries from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,42 @@
-import * as React from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
 const App = () => {
-  const [viewport, setViewport] = React.useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+  const [logEntries, setLogEntries] = useState([]);
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 39.8283,
+    longitude: -98.5795,
+    zoom: 3,
   });
+
+  useEffect(() => {
+    const getAllLogs = async () => {
+      const response = await fetch('http://localhost:3001/api/logs');
+      const logsData = await response.json();
+      setLogEntries(logsData);
+    };
+    getAllLogs();
+  }, []);
 
   return (
     <ReactMapGL
       {...viewport}
-      width="100%"
-      height="100%"
-      onViewportChange={(viewport) => setViewport(viewport)}
-    />
+      mapStyle="mapbox://styles/technogicksta/cksrkyy9u29ck17o3ueyx3d7n"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      onViewportChange={(viewport) => setViewport(viewport)}>
+      {logEntries.map((entry) => (
+        <Marker
+          key={entry._id}
+          latitude={entry.latitude}
+          longitude={entry.longitude}
+          offsetLeft={-20}
+          offsetTop={-10}>
+          <div>{entry.title}</div>
+        </Marker>
+      ))}
+    </ReactMapGL>
   );
 };
 
